4e5788e29087d47a9405d658763973a564d4550e
Tina <tinaliu@illinois.edu>
2018-02-06 16:40:19 -0600

    WIP project.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,33 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
-    public static void main(String[] unused) {
-        System.out.println("Beep.");
+
+    public static void main(final String[] unused) {
+        Scanner lineScanner = new Scanner(System.in);
+        System.out.println("Enter a URL: ");
+        String url = lineScanner.nextLine();
+        System.out.println("Enter a word to look for: ")
+        String lookFor = lineScanner.nextLine();
+        // WIP
+    }
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
